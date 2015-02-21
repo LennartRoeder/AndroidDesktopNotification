10303377c2c8c333418e7a1a797ad09fb52c7c75
Lennart K <lennart.k@thinking-aloud.eu>
2015-02-21 12:47:39 +0100

    Add a filter for certain notification spam

--- AndroidApp/src/main/java/eu/thinking_aloud/notification/NotificationService.java
+++ AndroidApp/src/main/java/eu/thinking_aloud/notification/NotificationService.java
@@ -10,39 +10,51 @@ import android.support.v4.content.LocalBroadcastManager;
 
 public class NotificationService extends NotificationListenerService {
     Context context;
+
     @Override
     public void onCreate() {
         super.onCreate();
         context = getApplicationContext();
     }
+
     @Override
     public void onNotificationPosted(StatusBarNotification sbn) {
 
         Intent msgrcv = new Intent("Msg");
         Bundle extras = sbn.getNotification().extras;
 
+
         String title = extras.getString("android.title");
-        Log.i("Title",title);
-        msgrcv.putExtra("title", title);
 
-        if(extras.getCharSequence("android.text") != null) {
-            String text = extras.getCharSequence("android.text").toString();
-            Log.i("Text",text);
-            msgrcv.putExtra("text", text);
+        if (!title.startsWith("Privacy Guard active")
+                && !title.startsWith("Downloading podcast data")
+                && !title.startsWith("USB connected")) {
 
-        } else if(extras.getCharSequenceArray("android.textLines") != null) {
+            Log.i("Title", title);
+            msgrcv.putExtra("title", title);
+
+            if (extras.getCharSequence("android.text") != null) {
+                String text = extras.getCharSequence("android.text").toString();
+
+
+                Log.i("Text", text);
+                msgrcv.putExtra("text", text);
+            }
+
+        } else if (extras.getCharSequenceArray("android.textLines") != null) {
             String text = "";
             for (CharSequence cs : extras.getCharSequenceArray("android.textLines")) {
-                text += cs +" ";
+                text += cs + " ";
             }
-            Log.i("Text",text);
+            Log.i("Text", text);
             msgrcv.putExtra("text", text);
         }
 
         LocalBroadcastManager.getInstance(context).sendBroadcast(msgrcv);
     }
+
     @Override
     public void onNotificationRemoved(StatusBarNotification sbn) {
-        Log.i("Msg","Notification Removed");
+        Log.i("Msg", "Notification Removed");
     }
 }
